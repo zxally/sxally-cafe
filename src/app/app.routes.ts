180df2eb8d1247ba2a6001cd61d3532dafc23cce
zxally <kosal.pouy@student.passerellesnumeriques.org>
2025-08-19 15:36:22 +0700

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact', component: ContactComponent },
+  
+  { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+
+}
